e7583e6970dadf68bd0999c31432f1e5dc5de0d2
jjband <abcdefg>
2023-10-05 21:49:51 +0900

    바탕에 대머리 추가와 대머리 변신 수정 필요

--- pygame모음집/rain.py
+++ pygame모음집/rain.py
@@ -15,7 +15,13 @@ white_green = (204, 255, 204)
 color = red
 current_path = os.path.dirname(__file__)
 assets_path = os.path.join(current_path, "assets")
-대머리모음 = "대머리1.png"
+image_name = "대머리1.png"
+imgage1 = pygame.image.load(os.path.join(assets_path, "대머리1.png"))
+imgage2 = pygame.image.load(os.path.join(assets_path, "대머리2.png"))
+imgage3 = pygame.image.load(os.path.join(assets_path, "대머리3.png"))
+imgage4 = pygame.image.load(os.path.join(assets_path, "대머리4.png"))
+imgage5 = pygame.image.load(os.path.join(assets_path, "대머리5.png"))
+ground_imgae = pygame.image.load(os.path.join(assets_path, "terrain.png"))
 
 
 class Life():
@@ -26,19 +32,30 @@ class Life():
     def set_life(self, life2):
         self.life += life2
         return self.life
+    
 class Player():
     def __init__(self):
-        self.image = pygame.image.load(os.path.join(assets_path, 대머리모음))
+        self.image = pygame.image.load(os.path.join(assets_path, image_name))
         self.radius = 15
         self.rect = self.image.get_rect()
         self.width = self.image.get_rect().width
         self.height = self.image.get_rect().height
-        self.score = 0
-        self.reset()
-
-    def reset(self):
+        self.reset(Life(5), "대머리1.png")
+    def reset(self, life_obj, image_name):
         self.rect.x = screen_w // 2 - self.width // 2 
-        self.rect.y = screen_h - self.height // 2 -50
+        self.rect.y = screen_h - self.height // 2 - 50
+        life = life_obj.get_life()
+        print(life)
+        if life == 4:
+            image_name = "대머리2.png"
+        elif life == 3:
+            image_name = "대머리3.png"
+        elif life == 2:
+            image_name = "대머리4.png"
+        elif life == 1:
+            image_name = "대머리5.png"
+
+        self.image = pygame.image.load(os.path.join(assets_path, image_name))
         
         
     def move(self, direction):
@@ -67,45 +84,24 @@ class Enemy():
     def check_crush(self, player, life):
         if self.rect.colliderect(player.rect):
             life.set_life(-1)
+            player.life -=1
             self.reset()
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, self.rect.center, self.radius)
 
-class Coin:
-    def __init__(self):
-        self.initialize_rect()
-        
-    def initialize_rect(self):  # 위치와 충돌 영역 초기화
-        self.x = random.randint(50, 750)
-        self.y = 0
-        self.speed = 3
-        self.radius = 10
-        self.rect = pygame.Rect(self.x - self.radius, self.y - self.radius,
-                                self.radius * 2,self.radius * 2)
-        
-    def draw(self, screen):
-        pygame.draw.circle(screen, (255,255,0), (self.x,self.y), self.radius)
 
-    def move(self):
-        self.y += self.speed
 
-    def check_crush(self,player):
-        if player.rect.colliderect(self.rect):  
-            player.score +=1  
-            self.initialize_rect()  
+
+
         
             
-def display_score(screen, font,score ):  
-    score_text = font.render(f"Coin: {score}", True,(0 ,0 ,0))
-    screen.blit(score_text,(screen_w -200 ,10))
 
 def display_life(screen, font, life):
     text = font.render(f"머리카락 수: {life.get_life()}", True, (0, 0, 0))
     screen.blit(text, (10, 10))
 
 def display_timer(screen, font, start_time):
-
 
 
     milliseconds = pygame.time.get_ticks() - start_time
@@ -120,10 +116,11 @@ def display_story(screen, font):
     story_text1 = font.render("탈모인에게 비는 매우 위험한 존재입니다.", True, (0, 0, 0))
     story_text2 = font.render("특히 산성비는 더욱 위험하지요.", True, (0, 0, 0))
     story_text3 = font.render("우리 탈모친구가 비를 피할수 있게 도와주세요!", True, (0, 0 ,255))
+    대머리말 = font.render("도와줘ㅠㅠ", True, (0,0,255))
     screen.blit(story_text1, (20,200))
     screen.blit(story_text2, (20,260))
     screen.blit(story_text3, (20,320))
-    
+    screen.blit(대머리말, (460,590))
 
 
 
@@ -135,7 +132,7 @@ def main():
     clock = pygame.time.Clock()
     enemy_count = 10
     player = Player()
-    life_font = pygame.font.Font(None, 36)
+    life_font = pygame.font.SysFont("Batang",30,0,0)
     timer_font = pygame.font.Font(None, 36)
     story_font = pygame.font.SysFont("Malgun Gothic", 36, 0,0)
     score_font = pygame.font.Font(None, 36)
@@ -149,6 +146,7 @@ def main():
     best_time = 0
     start_time = None 
     enemies = [Enemy()for _ in range(enemy_count)]
+    player.reset(Life(5), "대머리1.png")
     color = red
     running = True
     menu = True
@@ -169,8 +167,7 @@ def main():
                     elif show_story:
                         show_story = False
                         start_time = pygame.time.get_ticks()
-        if life == Life(4):
-            대머리모음 = "대머리2.png"
+
 
         keys = pygame.key.get_pressed()
         if not menu and not show_story:
@@ -180,32 +177,27 @@ def main():
                 player.move(1)
 
         if not menu and not show_story and pygame.time.get_ticks() - spawn_time > 3000:
-            coins.append(Coin())
             spawn_time = pygame.time.get_ticks()
 
-        for coin in coins[:]:
-            coin.move()
 
-            if coin.y > screen_h:
-                coins.remove(coin)
-                
-            else:
-                coin.draw(screen)
 
-                if player.rect.colliderect(coin.rect):
-                    player.score += 1
-                    coins.remove(coin)
 
                 
         if menu:
             screen.fill(yellow_orange)
-            player.draw(screen)
+            screen.blit(imgage1, (270,600))
+            screen.blit(imgage2, (340,600))
+            screen.blit(imgage3, (410,600))
+            screen.blit(imgage4, (480,600))
+            screen.blit(imgage5, (550,600))
             screen.blit(menu_text, (225,400))
             screen.blit(menu_text1, (200,175))
 
         elif show_story:        
             screen.fill(white_green)
             display_story(screen, story_font)
+            screen.blit(imgage5, (390,600))
+
 
         else:
             for enemy in enemies:
@@ -213,14 +205,17 @@ def main():
                 enemy.check_crush(player, life)
 
             screen.fill(sky_blue)
+            screen.blit(ground_imgae, (528,590))
+            screen.blit(ground_imgae, (0,555))
+
             player.draw(screen)
+            
 
 
             for enemy in enemies:
                 enemy.draw(screen)
 
             display_life(screen, life_font, life)
-            display_score(screen,score_font, player.score)
 
             if start_time is not None:  
                 display_timer(screen,timer_font,start_time)
@@ -232,7 +227,6 @@ def main():
        
                 for enemy in enemies: 
                     enemy.reset()
-
                 
         pygame.display.flip()
         clock.tick(120)
